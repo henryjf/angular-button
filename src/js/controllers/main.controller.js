7a1edf412806a826236821d972436b3f8383f337
JEFF HENRY <henry_jf@hotmail.com>
2016-05-14 15:56:52 -0400

    added button with likes feature

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -1,11 +1,7 @@
 function MainController($scope) {
 
   $scope.allTasks = [];
-
-  $scope.addTask = function(taskString) {
-  $scope.allTasks.push(taskString);
-  $scope.task = '';
-}
+  $scope.buttonStatus = 'likes';
 
 }
 
