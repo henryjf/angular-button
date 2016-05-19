ad5a4101000bf04fb976f09499e85a226a839ade
JEFF HENRY <henry_jf@hotmail.com>
2016-05-19 14:43:11 -0400

    change controller file to use scope and conditionals to make button functional

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -1,16 +1,22 @@
 function MainController($scope) {
 
-  $scope.buttonStatus = '0 Likes';
-  $scope.clicked = function(x) {
-    // if (clicks === 1)
-    $scope.buttonStatus = '1 Like';
+  //starts with 0 clicks
+   $scope.buttonStatus = 0;
 
-  if (x > 1)
-    $scope.buttonStatus = x + ' Likes';
-  }
+   $scope.label = 'likes';
+
+  //register button click which
+   $scope.clicked= function() {
+     $scope.buttonStatus = $scope.buttonStatus + 1;
+     //when buttonStatus=1, label should be 'like'
+     if ($scope.buttonStatus === 1) {
+       $scope.label = 'like';
+     } else {
+       $scope.label = 'likes';
+     }
+   }
 
 }
-
 
 
 export { MainController };
